64ee9975fc5d04dbf0468b9e522979802f718155
Meryem Sel <mzbasel637936@gmail.com>
2022-10-05 13:29:06 +0100

    Controllers test

---
+++ src/test/java/com/meryem/app/rest/ControllersShould.java
@@ -0,0 +1,13 @@
+package com.meryem.app.rest;
+
+import com.meryem.app.rest.controller.Controllers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    @Test public void
+    return_welcome(){
+        Controllers controllers = new Controllers();
+        Assertions.assertEquals("welcome", controllers.getPage());
+    }
+}
