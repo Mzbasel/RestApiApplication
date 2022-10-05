307a0bc7520bb2bc55a7a4c27caab4cb42f6b09b
Meryem Sel <mzbasel637936@gmail.com>
2022-10-05 13:37:41 +0100

    SpringBootTest annotation added

--- src/test/java/com/meryem/app/rest/ControllersShould.java
+++ src/test/java/com/meryem/app/rest/ControllersShould.java
@@ -3,12 +3,14 @@ package com.meryem.app.rest;
 import com.meryem.app.rest.controller.Controllers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ControllersShould {
     @Test public void
     return_welcome(){
         Controllers controllers = new Controllers();
-        Assertions.assertEquals("welcome", controllers.getPage());
+        Assertions.assertEquals("   Welcome", controllers.getPage());
     }
 
     @Test public void
